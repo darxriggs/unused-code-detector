5c0214d4c0e7e699feff33a4d2eaefc393afef73
evernat <evernat@free.fr>
2015-12-28 01:10:02 +0100

    remove useless code

--- src/main/java/org/jenkinsci/unusedcode/Reports.java
+++ src/main/java/org/jenkinsci/unusedcode/Reports.java
@@ -1,6 +1,5 @@
 package org.jenkinsci.unusedcode;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -19,7 +18,7 @@ public class Reports {
         log("");
 
         log("<h3 id=unusedMethods>Unused methods in Jenkins</h3>");
-        final Set<String> methods = new TreeSet<>(filterOnJenkins(indexer.getMethods()));
+        final Set<String> methods = new TreeSet<>(indexer.getMethods());
         for (final String method : methods) {
             log(formatMethod(method));
         }
@@ -29,19 +28,6 @@ public class Reports {
         log(methods.size()
                 + " unused methods in Jenkins, except getters, setters, except deprecated classes and methods");
         log("Unused deprecated classes, methods and fields are listed in the <a href='https://ci.jenkins-ci.org/view/Infrastructure/job/infra_deprecated-usage-in-plugins/ws/target/output.html#deprecatedApiNotUsed'> deprecated-usage-in-plugins job </a>");
-    }
-
-    private static Set<String> filterOnJenkins(Set<String> methods) {
-        final Set<String> filtered = new LinkedHashSet<>();
-        for (final String method : methods) {
-            final boolean jenkinsClass = method.startsWith("jenkins/")
-                    || method.startsWith("hudson/");
-            // || method.startsWith("org/kohsuke/") : org/kohsuke/stapler/ are false positives ?
-            if (jenkinsClass) {
-                filtered.add(method);
-            }
-        }
-        return filtered;
     }
 
     private static String formatMethod(String method) {
